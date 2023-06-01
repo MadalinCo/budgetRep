7b0ba3a1e8bb505032467394757f47266da4763f
MadalinCo <colibamadalin@gmail.com>
2023-06-01 14:00:42 +0300

    first commit

---
+++ src/main/java/org/fasttrackit/budget/BudgetApplication.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.budget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetApplication.class, args);
+	}
+
+}
